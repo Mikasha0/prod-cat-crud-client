47f8246ba5ab74d6df5854fe04a3e8ff57e22ae1
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-07-27 14:46:01 +0545

    types declared where necessary

--- app/component/addCategory.tsx
+++ app/component/addCategory.tsx
@@ -1,8 +1,8 @@
 import { Form } from "@remix-run/react";
+import { AddCategoryProps } from "~/types/add-category-prop.types";
 import { Status } from "~/types/z.schema";
 
-
-export default function AddCategory({ toggleModal }: any) {
+export default function AddCategory({ toggleModal }: AddCategoryProps) {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div
@@ -33,7 +33,7 @@ export default function AddCategory({ toggleModal }: any) {
               name="status"
               className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             >
-              {Status.map((status) => (
+              {Status.map((status:string) => (
                 <option key={status} value={status}>
                   {status}
                 </option>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,14 @@
 import {
-  LoaderArgs,
-  type ActionArgs,
-  type V2_MetaFunction
+  type ActionArgs
 } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-import { Status } from "~/types/z.schema";
-import { db } from "~/utils/db.server";
 import { useState } from "react";
 import { createCategoryAction } from "~/action/createCategoryAction";
 import { createProductAction } from "~/action/createProductAction";
-import {loader as getCategory} from "~/loader/getCategory"
 import AddCategory from "~/component/addCategory";
-
+import { loader as getCategory } from "~/loader/getCategory";
+import { CategoryIdAndName } from "~/types/categoryIdAndName-types";
+import { Status } from "~/types/z.schema";
 export const loader = async ()=>{
   return getCategory();
 }
@@ -51,7 +48,7 @@ export default function Index() {
               name="categoryId"
               className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             >
-              {data.map((category:any) => (
+              {data.map((category:CategoryIdAndName) => (
                 <option key={category.id} value={category.id}>
                   {category.name}
                 </option>
@@ -60,7 +57,7 @@ export default function Index() {
           </div>
           <button
             type="button"
-            onClick={toggleModal} // Show the modal when the button is clicked
+            onClick={toggleModal}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add Category
